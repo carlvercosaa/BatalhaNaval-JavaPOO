065337fb2769db957d20afb6cf5bcbb7a709b6d4
carlvercosaa <113189009+carlvercosaa@users.noreply.github.com>
2022-12-26 22:59:15 -0300

    Navio de 4 canos

--- src/enity/navioDe4Canos.java
+++ src/enity/navioDe4Canos.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package enity;
+import tabuleiro.tabuleiro;
 
-/**
- *
- * @author andre
- */
-public class navioDe4Canos {
+public class navioDe4Canos extends embarcacao{
+    protected int tamanho = 4;
+   
+    public navioDe2Canos(enity.jogador jogador, int numeroJogador) {
+        super(jogador, numeroJogador, 4);
+    }
+
+    @Override
+    public String toString() {
+        return "navioDe1Cano{" + '}';
+    }
+   
+    public void escolherPosicaoDaEmbarcacao(tabuleiro  tabuleiro) {
+		// TODO Auto-generated method stub
+        super.escolherPosicaoDaEmbarcacao(tabuleiro);
+    }
     
 }
