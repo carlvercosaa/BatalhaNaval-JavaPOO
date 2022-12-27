929bc52cd37e45a5d32f22982523adf368a15d8e
carlvercosaa <113189009+carlvercosaa@users.noreply.github.com>
2022-12-26 22:54:16 -0300

    Implementado Navio de 1 cano

--- src/enity/navioDe1Cano.java
+++ src/enity/navioDe1Cano.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 import tabuleiro.tabuleiro;
 
 public class navioDe1Cano extends embarcacao{
- 
+    protected int tamanho = 1;
+    
+    public navioDe1Cano(jogador jogador, int numeroJogador) {
+        super(jogador, numeroJogador, 1);
+    }
+
+    @Override
+    public String toString() {
+        return "navioDe1Cano{" + '}';
+    }
+   
+    public void escolherPosicaoDaEmbarcacao(tabuleiro  tabuleiro) {
+		// TODO Auto-generated method stub
+        super.escolherPosicaoDaEmbarcacao(tabuleiro);
+    }   
 }
