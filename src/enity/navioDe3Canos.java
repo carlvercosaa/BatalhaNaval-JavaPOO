5b8665b9b5a224398b617b91bc2df12a1712b87c
carlvercosaa <113189009+carlvercosaa@users.noreply.github.com>
2022-12-26 22:57:34 -0300

    Navio de 3 canos

--- src/enity/navioDe3Canos.java
+++ src/enity/navioDe3Canos.java
@@ -1,5 +1,21 @@
 package enity;
+import tabuleiro.tabuleiro;
 
-public class navioDe3Canos {
+public class navioDe3Canos extends embarcacao {
+    protected int tamanho = 3;
+   
+    public navioDe2Canos(enity.jogador jogador, int numeroJogador) {
+        super(jogador, numeroJogador, 3);
+    }
+
+    @Override
+    public String toString() {
+        return "navioDe1Cano{" + '}';
+    }
+   
+    public void escolherPosicaoDaEmbarcacao(tabuleiro  tabuleiro) {
+		// TODO Auto-generated method stub
+        super.escolherPosicaoDaEmbarcacao(tabuleiro);
+    }
     
 }
