6cdcbb024037eba19eed90c329e7173e26755a9d
André Luís <andreluisbastos.2020@gmail.com>
2022-12-26 22:46:48 -0300

    subindo navioDeCanos1, falta implementar

--- src/enity/navioDe1Cano.java
+++ src/enity/navioDe1Cano.java
@@ -2,4 +2,6 @@ package enity;
 import java.util.Scanner;
 import tabuleiro.tabuleiro;
 
+public class navioDe1Cano extends embarcacao{
+ 
 }
--- src/enity/navioDe2Canos.java
+++ src/enity/navioDe2Canos.java
@@ -3,6 +3,5 @@ package enity;
 import tabuleiro.tabuleiro;
 
 public class navioDe2Canos extends embarcacao{
-
-    
+ 
 }
