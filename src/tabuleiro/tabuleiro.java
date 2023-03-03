30fea20a3b44c14a3a27ce1893b2d9406f3992f3
carlvercosaa <cvv@poli.br>
2023-03-03 19:56:18 -0300

    Adicionando verificação de plotagem

--- src/tabuleiro/tabuleiro.java
+++ src/tabuleiro/tabuleiro.java
@@ -1,37 +1,20 @@
 package tabuleiro;
 
-import enity.jogador;
-
-public class tabuleiro {
-    jogador jogador;
+public class Tabuleiro {
     private static int[][] tabuleiroJogador = new int[10][10];
- 
-    
-    public jogador getJogador() {
-        return jogador;
-    }
-
-    public void setJogador(jogador jogador) {
-        this.jogador = jogador;
-    }
 
     public static int[][] getTabuleiroJogador() {
         return tabuleiroJogador;
     }
 
-    public static void setTabuleiroJogador(int[][] tabuleiroJogador1) {
-        tabuleiro.tabuleiroJogador = tabuleiroJogador1;
-    }
-
     public static int[][] inicializaGrelha(){
             for(int linha=0 ; linha < 10 ; linha++ ){
                 for(int coluna=0 ; coluna < 10 ; coluna++ ){
-                    tabuleiro.tabuleiroJogador[linha][coluna]=-1;
+                    Tabuleiro.tabuleiroJogador[linha][coluna]=-1;
                 }
             }
-            return tabuleiro.tabuleiroJogador;
+            return Tabuleiro.tabuleiroJogador;
         }
-	
         
 	 public static void mostraGrelhaDeAtaque(int[][] tabuleiro){
 	        System.out.println("\t0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9");
@@ -54,19 +37,59 @@ public class tabuleiro {
 
     }
 
-    public void verificarEPlotarVertical(int tamanho, int  linha, int  coluna){
+    public void verificarEPlotarVerticalParaBaixo(int tamanho, int  linha, int  coluna){
         int contador = 0;
-        for(int c = 0; c < tamanho; c++){
+        for(int c = 0; c < tamanho; c++){                       //Verifica se pode colocar
+            if(this.tabuleiroJogador[linha+c][coluna] == -1){
+                contador += 1;
+            }
+        }
+        if(contador == tamanho){
+            for(int c = 0; c < tamanho; c++){                 //Coloca o navio no tabuleiro
+                this.tabuleiroJogador[linha+c][coluna] = 0;
+            }
+        }
+    }
+    
+    public void verificarEPlotarVerticalParaCima(int tamanho, int linha, int coluna) {
+    	int contador = 0;
+    	for(int c = 0; c < tamanho; c++){                       //Verifica se pode colocar
+            if(this.tabuleiroJogador[linha-c][coluna] == -1){
+                contador += 1;
+            }
+        }
+    	if(contador == tamanho){
+            for(int c = 0; c < tamanho; c++){                 //Coloca o navio no tabuleiro
+                this.tabuleiroJogador[linha-c][coluna] = 0;
+            }
+        }
+    }
+    
+    public void verificarEPlotarHorizontalParaDireita(int tamanho, int linha, int coluna) {
+    	int contador = 0;
+    	for(int c = 0; c < tamanho; c++){                       //Verifica se pode colocar
             if(this.tabuleiroJogador[linha][coluna+c] == -1){
                 contador += 1;
             }
         }
-        
-        if(contador == tamanho){
-            for(int c = 0; c < tamanho; c++){
+    	if(contador == tamanho){
+            for(int c = 0; c < tamanho; c++){                 //Coloca o navio no tabuleiro
                 this.tabuleiroJogador[linha][coluna+c] = 0;
             }
         }
-        
+    }
+ 
+    public void verificarEPlotarHorizontalParaEsquerda(int tamanho, int linha, int coluna) {
+    	int contador = 0;
+    	for(int c = 0; c < tamanho; c++){                       //Verifica se pode colocar
+            if(this.tabuleiroJogador[linha][coluna-c] == -1){
+                contador += 1;
+            }
+        }
+    	if(contador == tamanho){
+            for(int c = 0; c < tamanho; c++){                 //Coloca o navio no tabuleiro
+                this.tabuleiroJogador[linha][coluna-c] = 0;
+            }
+        }
     }
 }
