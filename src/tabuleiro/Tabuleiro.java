de481f6643714208332135a9c33726ec9d5bf3ad
carlvercosaa <cvv@poli.br>
2023-03-08 22:19:25 -0300

    Adicionando o método de interação com a grelha de ataque

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,10 +1,7 @@
 package tabuleiro;
 
-import enity.Jogador;
-
 public class Tabuleiro {
     private int[][] tabuleiroJogador = new int[10][10];
-    private int[][] tabuleiroJogadorAtaque = new int[10][10];
     
     public Tabuleiro() {
     	this.inicializaGrelha();
@@ -15,7 +12,6 @@ public class Tabuleiro {
             for(int linha=0 ; linha < 10 ; linha++ ){
                 for(int coluna=0 ; coluna < 10 ; coluna++ ){
                     this.tabuleiroJogador[linha][coluna]=-1;
-                    this.tabuleiroJogadorAtaque[linha][coluna]=-1;
                 }
         }
     }
--- src/tabuleiro/TabuleiroDeAtaque.java
+++ src/tabuleiro/TabuleiroDeAtaque.java
@@ -1,5 +1,23 @@
 package tabuleiro;
 
 public class TabuleiroDeAtaque extends Tabuleiro {
-
+	
+	public void mostraGrelhaDeAtaque(){
+	 	System.out.println("\t0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9");
+	 	System.out.println();
+	 	for(int i=0 ; i < 10 ; i++ ){
+            System.out.print((i)+"");
+            for(int j=0 ; j < 10 ; j++ ){
+                if(super.getTabuleiroJogador()[i][j]== 3){
+                    System.out.print("\t"+"A");
+                }else if(super.getTabuleiroJogador()[i][j]== 1) {
+                    System.out.print("\t"+"N");
+                }else if(super.getTabuleiroJogador()[i][j]== 2) {
+                	System.out.print("\t"+"P");
+                }
+                
+            }
+            System.out.println();
+        }
+	}
 }
