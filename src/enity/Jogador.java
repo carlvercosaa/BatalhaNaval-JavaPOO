3a23ac071b8d8cab22edd97a98152370bfdd6384
André Luís <andre.luis.2018@outlook.com>
2023-04-24 21:10:02 -0300

    retirando algumas importações desnecessárias

--- src/enity/Jogador.java
+++ src/enity/Jogador.java
@@ -1,17 +1,11 @@
 package enity;
 
-import java.util.Scanner;
 
 import ExceptionsDaBatalha.DirecaoInvalidaException;
-import ExceptionsDaBatalha.EixoInvalidoException;
 import ExceptionsDaBatalha.ForaDoIndiceException;
 import ExceptionsDaBatalha.ForaDosLimitesDoArrayException;
 import ExceptionsDaBatalha.PlotagemException;
 import logica.LogicaBatalhaNaval;
-
-import java.util.Arrays;
-import java.util.List;
-
 import tabuleiro.TabuleiroDeAtaque;
 import tabuleiro.TabuleiroDeDefesa;
 
